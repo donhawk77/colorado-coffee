e615691b450501911105f6a07960f8791151de12
donhawk77 <don.hawk77@gmail.com>
2026-04-07 19:20:52 -0500

    Fix links and unique espresso image

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/colorado-coffee/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        heritage: resolve(__dirname, 'heritage.html')
+      }
+    }
+  }
 });
